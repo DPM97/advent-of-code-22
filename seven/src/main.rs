b85ded3ff60f685c2b904d0232da368d4a4f96d3
Dylan Maloy <dmaloy97@gmail.com>
2022-12-07 17:30:56 +1100

    speed

--- seven/src/main.rs
+++ seven/src/main.rs
@@ -1,14 +1,13 @@
-use std::collections::{HashMap, HashSet};
-use std::io::Error;
+use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::time::Instant;
 use std::{
     fs::File,
     io::{self, BufRead},
 };
 
 struct TreeNode {
-    name: String,
     size: u32,
-    children: HashSet<String>,
     parent: Option<String>,
     is_dir: bool,
 }
@@ -26,14 +25,14 @@ fn main() {
         })
         .collect::<Vec<String>>();
 
+    let now = Instant::now();
+
     let mut nodes = HashMap::<String, TreeNode>::from([(
         "".to_string(),
         TreeNode {
-            name: "".to_string(),
             is_dir: true,
             size: 0,
             parent: None,
-            children: HashSet::<String>::new(),
         },
     )]);
 
@@ -48,87 +47,71 @@ fn main() {
                         cur_path = nodes.get(&cur_path).unwrap().parent.clone().unwrap();
                     }
                     dir => {
-                        let mut new_path = cur_path.clone();
-                        new_path.push_str("/");
-                        new_path.push_str(dir);
+                        let new_path = format!("{cur_path}/{dir}");
 
                         nodes.entry(new_path.clone()).or_insert(TreeNode {
                             is_dir: true,
-                            name: dir.to_string(),
                             size: 0,
-                            children: HashSet::new(),
                             parent: Some(cur_path.clone()),
-                        });
-
-                        nodes.entry(cur_path.clone()).and_modify(|n| {
-                            n.children.insert(new_path.clone());
                         });
 
                         cur_path = new_path;
                     }
                 },
-                "ls" => { /* skip (next line we can actually process) */ }
-                _ => {
-                    panic!("invalid command!")
+                _ => {}
+            },
+            _ => match l[0] {
+                "dir" => {}
+                x => {
+                    let new_path = format!("{cur_path}/{}", l[1]);
+                    let file_size = x.parse::<u32>().unwrap();
+
+                    let mut parent = nodes
+                        .entry(new_path)
+                        .or_insert(TreeNode {
+                            is_dir: false,
+                            size: file_size,
+                            parent: Some(cur_path.clone()),
+                        })
+                        .parent
+                        .clone();
+
+                    while let Some(p) = parent {
+                        let n = nodes.get_mut(&p).unwrap();
+                        n.size += file_size;
+                        parent = n.parent.clone();
+                    }
                 }
             },
-            _ => {
-                match l[0] {
-                    "dir" => {
-                        // nada
-                    }
-                    x => {
-                        let mut new_path = cur_path.clone();
-                        new_path.push_str("/");
-                        new_path.push_str(l[1]);
-
-                        let file_size = x.parse::<u32>().unwrap();
-
-                        let mut parent = nodes
-                            .entry(new_path)
-                            .or_insert(TreeNode {
-                                is_dir: false,
-                                name: l[1].to_string(),
-                                size: file_size,
-                                children: HashSet::new(),
-                                parent: Some(cur_path.clone()),
-                            })
-                            .parent
-                            .clone();
-
-                        while let Some(p) = parent {
-                            let n = nodes.get_mut(&p).unwrap();
-                            n.size += file_size;
-                            parent = n.parent.clone();
-                        }
-                    }
-                }
-            }
         }
     }
 
+    println!("tree built in {:?}", now.elapsed());
+
+    let now = Instant::now();
     part_one(&nodes);
+    println!("part 1: {} ({:?})", part_one(&nodes), now.elapsed());
+    let now = Instant::now();
     part_two(&nodes);
+    println!("part 2: {} ({:?})", part_two(&nodes), now.elapsed());
 }
 
-fn part_one(nodes: &HashMap<String, TreeNode>) {
-    println!(
-        "sum: {}",
-        nodes
-            .iter()
-            .filter_map(|(_, node)| {
-                if !node.is_dir || node.size > 100000 {
-                    return None;
-                }
-                Some(node.size as u64)
-            })
-            .sum::<u64>()
-    );
+fn part_one(nodes: &HashMap<String, TreeNode>) -> u64 {
+    nodes
+        .iter()
+        .filter_map(|(_, node)| {
+            if !node.is_dir || node.size > 100000 {
+                return None;
+            }
+            Some(node.size as u64)
+        })
+        .sum::<u64>()
 }
 
-fn part_two(nodes: &HashMap<String, TreeNode>) {
+fn part_two(nodes: &HashMap<String, TreeNode>) -> u32 {
     let total_space = 70000000;
     let needed_space = 30000000;
+
     let space_used = nodes
         .iter()
         .filter_map(|(_, node)| {
@@ -141,11 +124,22 @@ fn part_two(nodes: &HashMap<String, TreeNode>) {
 
     let target = (needed_space - (total_space - space_used)) as u32;
 
-    let mut sizes = nodes
+    nodes
         .values()
-        .filter(|n| n.is_dir && n.size >= target)
-        .map(|n| n.size)
-        .collect::<Vec<u32>>();
-    sizes.sort();
-    println!("smallest: {}", sizes[0]);
+        .min_by(|&a, &b| match (a.is_dir, b.is_dir) {
+            (false, false) => Ordering::Equal,
+            (true, false) => Ordering::Less,
+            (false, true) => Ordering::Greater,
+            (true, true) => match (a.size > target, b.size > target) {
+                (false, false) => Ordering::Equal,
+                (true, false) => Ordering::Less,
+                (false, true) => Ordering::Greater,
+                (true, true) => match a.size < b.size {
+                    true => Ordering::Less,
+                    false => Ordering::Greater,
+                },
+            },
+        })
+        .unwrap()
+        .size
 }
